534d1f7f8be081147ef958fe79f45cd013a10d3a
Keviinplz <kevin.pinochet@ug.uchile.cl>
2023-11-12 21:06:14 -0300

    feat (docs): Added docs

--- dentalink/client.py
+++ dentalink/client.py
@@ -15,7 +15,18 @@ from dentalink.schemas.sucursales import DentalinkSucursalResponse
 
 
 class DentalinkClient:
-    def __init__(self, url: str, *, token: str):
+    """
+    Cliente de la API de Dentalink
+    https://api.dentalink.healthatom.com/docs
+
+    Parámetros:
+        url: String que representa la URL de conexión a la API (Por defecto: https://api.dentalink.healthatom.com/api/v1)
+        token: String que representa el token de autorización (Ver: https://api.dentalink.healthatom.com/docs/#autenticacin)
+    """
+
+    def __init__(
+        self, url: str = "https://api.dentalink.healthatom.com/api/v1", *, token: str
+    ):
         session = requests.session()
         session.headers.update({"Authorization": f"Token {token}"})
         self._session = session
@@ -24,6 +35,19 @@ class DentalinkClient:
     def __make_uri(
         self, endpoint: str, query: Union[dict[str, Any], None] = None
     ) -> str:
+        """Retorna la uri final para la API
+
+        Se encarga de juntar la URL base de la API con el endpoint proporcionado.
+        A su vez realiza un encode usando 'urllib.parse.quote' de la query proporcionada
+        retornando la URI completa
+
+        Parámetros:
+            endpoint: Recurso a solicitar
+            query: Parámetro opcional que contiene los filtros a realizar en la petición (Ver: https://api.dentalink.healthatom.com/docs/#filtros-y-cursores)
+
+        Retorna:
+            Un string que representa la URI completa a donde solicitar la información
+        """
         if not query or len(query) == 0:
             return self._url + endpoint
 
@@ -44,6 +68,23 @@ class DentalinkClient:
         query: Union[DentalinkQuery, None] = None,
         data: Union[dict[str, Union[str, int]], None] = None,
     ) -> dict[str, Any]:
+        """Realiza una petición a la API
+
+        El método construye la URI a usar y envía la petición a la API
+        Se encarga de gestionar la respuesta y retorna los datos en formato JSON
+
+        Parámetros:
+            method: Método HTTP a utilizar (GET, POST, PUT, DELETE, etc...)
+            endpoint: Recurso a solicitar
+            query: Parámetro opcional que contiene los filtros a realizar en la petición (Ver: https://api.dentalink.healthatom.com/docs/#filtros-y-cursores)
+            data: Parámetro opcional que contiene los datos a enviar (se envían en formato JSON vía body de la petición)
+
+        Retorna:
+            Un diccionario con la respuesta de la petición
+
+        Errores:
+            dentalink.exceptions.DentalinkClientHTTPException: La respuesta de la API no fue satistactoria (el código no es 200)
+        """
         uri = self.__make_uri(endpoint, query=query.parse() if query else None)
         response = self._session.request(method, uri, json=data)
 
@@ -69,6 +110,21 @@ class DentalinkClient:
         id_sucursal: Union[int, None] = None,
         id_estado: Union[int, None] = None,
     ):
+        """Obtiene el listado de todas las citas de la clínica (ver: https://api.dentalink.healthatom.com/docs/#get-citas)
+
+        Parámetros:
+            start_date: Parámetro opcional que permite definir una fecha (datetime) como límite inferior (inclusivo).
+            end_date: Parámetro opcional que permite definir una fecha (datetime) como límiter superior (inclusivo).
+            id_surcursal: Entero positivo que representa la ID de una surcursal (ver: https://api.dentalink.healthatom.com/docs/#sucursales).
+            id_estado: Entero positivo que representa la ID de un estado de cita (ver: https://api.dentalink.healthatom.com/docs/#estados-de-cita).
+
+        Retorna:
+            Objeto `DentalinkResponse` con el resultado de la petición, donde el atributo `data` es
+            una lista de objetos `DentalinkCitaResponse`
+
+        Errores:
+            `dentalink.exceptions.DentalinkClientHTTPException`: La respuesta de la API no fue satistactoria (el código no es 200)
+        """
         endpoint = "/citas"
 
         q = (
@@ -97,6 +153,22 @@ class DentalinkClient:
         uso_interno: Union[bool, None] = None,
         habilitado: Union[bool, None] = None,
     ):
+        """Obtiene el listado de todos los estados de cita disponibles (ver: https://api.dentalink.healthatom.com/docs/#get-citasestados)
+
+        Parámetros:
+            nombre: String opcional que permite filtrar por nombre del estado de cita.
+            reservado: Boolean opcional que permite filtrar por estado de reservación.
+            anulacion: Boolean opcional que permite filtrar por estado de anulación.
+            uso_interno: Boolean opcional que permite filtrar por estado de uso interno.
+            habilitado: Boolean opcional que permite filtrar estados habilitados o no.
+
+        Retorna:
+            Objeto `DentalinkResponse` con el resultado de la petición, donde el atributo `data` es
+            una lista de objetos `DentalinkEstadoCitaResponse`
+
+        Errores:
+            `dentalink.exceptions.DentalinkClientHTTPException`: La respuesta de la API no fue satistactoria (el código no es 200)
+        """
         endpoint = "/citas/estados"
         q = (
             DentalinkQuery("nombre")
@@ -121,6 +193,20 @@ class DentalinkClient:
     def obtener_sucursales(
         self, *, nombre: Union[str, None] = None, habilitada: Union[bool, None] = None
     ):
+        """Obtiene el listado de todas las sucursales de la clínica (ver: https://api.dentalink.healthatom.com/docs/#get-sucursales)
+
+        Parámetros:
+            nombre: String opcional que permite filtrar por nombre de la sucursal.
+            habilitado: Boolean opcional que permite filtrar sucursales habilitadas o inhabilitadas.
+
+        Retorna:
+            Objeto `DentalinkResponse` con el resultado de la petición, donde el atributo `data` es
+            una lista de objetos `DentalinkSucursalResponse`
+
+        Errores:
+            `dentalink.exceptions.DentalinkClientHTTPException`: La respuesta de la API no fue satistactoria (el código no es 200)
+        """
+
         endpoint = "/sucursales"
         q = DentalinkQuery("nombre").eq(nombre).field("habilitada").eq(habilitada)
 
@@ -132,6 +218,18 @@ class DentalinkClient:
         )
 
     def obtener_cita_segun_id(self, id_cita: int):
+        """Obtiene una única cita según la ID de la cita (ver: https://api.dentalink.healthatom.com/docs/#get-citasid_cita)
+
+        Parámetros:
+            id_cita: Entero positivo que representa la ID de la cita a buscar.
+
+        Retorna:
+            Objeto `DentalinkResponse` con el resultado de la petición, donde el atributo `data` es
+            un objeto `DentalinkEstadoCitaResponse`
+
+        Errores:
+            `dentalink.exceptions.DentalinkClientHTTPException`: La respuesta de la API no fue satistactoria (el código no es 200)
+        """
         endpoint = f"/citas/{id_cita}"
 
         response = self._request("GET", endpoint)
@@ -150,8 +248,24 @@ class DentalinkClient:
         comentarios: Union[str, None] = None,
         flag_notificar_anulacion: Union[bool, None] = None,
     ):
+        """Actualiza la información de una cita según la ID de ésta (ver: https://api.dentalink.healthatom.com/docs/#put-citasid_cita)
+
+        Parámetros:
+            id_cita: Entero positivo que representa la ID de la cita a buscar.
+            duracion: Entero positivo opcional que representa la duración en minutos
+            id_estado: Entero positivo opcional que representa la ID de un estado de cita (ver: https://api.dentalink.healthatom.com/docs/#estados-de-cita).
+            comentarios: String opcional que representa un comentario adjunto a la cita.
+            flag_notificar_anulacion: Bool opcional que permite enviar un correo al paciente notificando la anulación.
+
+        Retorna:
+            Objeto `DentalinkResponse` con el resultado de la petición, donde el atributo `data` es
+            un objeto `DentalinkCitaResponse`
+
+        Errores:
+            `dentalink.exceptions.DentalinkClientHTTPException`: La respuesta de la API no fue satistactoria (el código no es 200)
+        """
         endpoint = f"/citas/{id_cita}"
-        data = {}
+        data: dict[str, Union[int, str, bool]] = {}
 
         if duracion:
             data["duracion"] = duracion
@@ -171,3 +285,6 @@ class DentalinkClient:
             links=response.get("links"),
             data=DentalinkCitaResponse(**response["data"]),
         )
+
+
+__all__ = ["DentalinkClient"]
--- dentalink/query.py
+++ dentalink/query.py
@@ -7,17 +7,63 @@ from dentalink.exceptions import (
 
 
 class DentalinkQuery:
+    """Construye un filtro para la API de Dentalink
+
+    La API define una mecánica de filtros para utilizar en las peticiones,
+    dicho filtros constan de un diccionario de campos que contienen operaciones.
+    Esta clase busca facilitar la creación de dicho filtros.
+
+    Parámetros
+        field: String que representa el campo inicial (opcional)
+
+    Módo de uso
+    ```py
+        >>> (DentalinkQuery().field('foo').eq(3)
+            .field('bar').gt(1).lt(3)
+            .field('now').eq(datetime(2023, 11, 12), dt_format='%Y-%m-%d')
+            .parse())
+        {
+            "foo": { "eq": 3 },
+            "bar": [{"gt": 1}, {"lt": 3}],
+            "now": { "eq": "2023-11-12" }
+        }
+    ```
+    """
+
     def __init__(self, field: Union[str, None] = None):
         self._query: dict[str, List[dict[str, str]]] = {}
-        self._last_field = None
+        self._last_field: Union[str, None] = None
 
         if field:
             self._set_field(field)
 
     def _cast_value_to_str(
-        self, value: Union[int, float, datetime, bool, str], dt_format: str
+        self,
+        value: Union[int, float, datetime, bool, str],
+        dt_format: Union[str, None] = None,
     ) -> str:
+        """Convierte el valor en un string
+
+        Formatea el valor según el tipo, si es un datetime, lo convertirá a string según el formato dado,
+        si es un booleano, lo convertirá en un string con la representación 0 o 1 (con 1 True), para el resto
+        de tipos lo convertirá en string.
+
+        Parámetros:
+            value: Valor a castear, puede ser entero, flotante, datetime, booleano o string.
+            dt_format: Parámetro opcional que representa el formato a considerar para castear un datetime.
+            En caso de que value sea datetime y este parámetro no se encuentre, arrojará un error.
+
+        Retorna:
+            String que corresponde al valor casteado
+
+        Errores:
+            `dentalink.exceptions.DentalinkClientQueryError`: Arrojado en caso de error con un campo no añadido o formato inválido.
+        """
         if isinstance(value, datetime):
+            if dt_format is None:
+                raise DentalinkClientQueryError(
+                    "Debes proporcionar un formato si el valor es un datetime"
+                )
             return value.strftime(dt_format)
 
         if isinstance(value, bool):
@@ -26,6 +72,11 @@ class DentalinkQuery:
         return str(value)
 
     def _set_field(self, field: str) -> None:
+        """Agrega un nuevo campo a la query
+
+        Parámetros
+            field: Nuevo campo a agregar
+        """
         self._last_field = field
         self._query[field] = []
 
@@ -35,6 +86,19 @@ class DentalinkQuery:
         value: Union[int, float, datetime, bool, str, None] = None,
         dt_format: str = "%Y-%m-%d",
     ) -> "DentalinkQuery":
+        """Agrega un nuevo filtro
+
+        Parámetros:
+            name: String que representa el nombre del operador (ver: https://api.dentalink.healthatom.com/docs/#filtros-y-cursores).
+            value: Parámetro opcional que contiene el valor del operador.
+            dt_format: Parámetro opcional que contiene el formato datetime a usar si es que el valor es un datetime.
+
+        Retorna:
+            Este mismo objeto, pero con el filtro añadido
+
+        Errores:
+            `dentalink.exceptions.DentalinkClientQueryError`: Arrojado en caso de que se intente agregar un filtro cuando no se ha configurado un campo.
+        """
         if self._last_field is None:
             raise DentalinkClientQueryError(
                 message="Debes establecer un campo usando .field() antes de agregar un filtro"
@@ -49,6 +113,14 @@ class DentalinkQuery:
         return self
 
     def field(self, field_name: str) -> "DentalinkQuery":
+        """Agrega un nuevo campo
+
+        Parámetros:
+            field_name: String que representa el nombre del campo a agregar.
+
+        Retorna:
+            Este mismo objeto, pero con el campo agregado
+        """
         if self._last_field == field_name:
             return self
 
@@ -60,6 +132,14 @@ class DentalinkQuery:
         value: Union[int, float, datetime, bool, str, None] = None,
         dt_format: str = "%Y-%m-%d",
     ) -> "DentalinkQuery":
+        """Implementación del operador `eq` (ver: https://api.dentalink.healthatom.com/docs/#filtros-y-cursores)
+
+        Retorna:
+            Este mismo objeto, pero con el filtro `eq` agregado
+
+        Errores:
+            `dentalink.exceptions.DentalinkClientQueryError`: Arrojado en caso de que se intente agregar un filtro cuando no se ha configurado un campo.
+        """
         return self._add_filter("eq", value, dt_format)
 
     def neq(
@@ -67,6 +147,14 @@ class DentalinkQuery:
         value: Union[int, float, datetime, bool, str, None] = None,
         dt_format: str = "%Y-%m-%d",
     ) -> "DentalinkQuery":
+        """Implementación del operador `neq` (ver: https://api.dentalink.healthatom.com/docs/#filtros-y-cursores)
+
+        Retorna:
+            Este mismo objeto, pero con el filtro `neq` agregado
+
+        Errores:
+            `dentalink.exceptions.DentalinkClientQueryError`: Arrojado en caso de que se intente agregar un filtro cuando no se ha configurado un campo.
+        """
         return self._add_filter("neq", value, dt_format)
 
     def gt(
@@ -74,6 +162,14 @@ class DentalinkQuery:
         value: Union[int, float, datetime, None] = None,
         dt_format: str = "%Y-%m-%d",
     ) -> "DentalinkQuery":
+        """Implementación del operador `gt` (ver: https://api.dentalink.healthatom.com/docs/#filtros-y-cursores)
+
+        Retorna:
+            Este mismo objeto, pero con el filtro `gt` agregado
+
+        Errores:
+            `dentalink.exceptions.DentalinkClientQueryError`: Arrojado en caso de que se intente agregar un filtro cuando no se ha configurado un campo.
+        """
         return self._add_filter("gt", value, dt_format)
 
     def gte(
@@ -81,6 +177,14 @@ class DentalinkQuery:
         value: Union[int, float, datetime, None] = None,
         dt_format: str = "%Y-%m-%d",
     ) -> "DentalinkQuery":
+        """Implementación del operador `gte` (ver: https://api.dentalink.healthatom.com/docs/#filtros-y-cursores)
+
+        Retorna:
+            Este mismo objeto, pero con el filtro `gte` agregado
+
+        Errores:
+            `dentalink.exceptions.DentalinkClientQueryError`: Arrojado en caso de que se intente agregar un filtro cuando no se ha configurado un campo.
+        """
         return self._add_filter("gte", value, dt_format)
 
     def lt(
@@ -88,6 +192,14 @@ class DentalinkQuery:
         value: Union[int, float, datetime, None] = None,
         dt_format: str = "%Y-%m-%d",
     ) -> "DentalinkQuery":
+        """Implementación del operador `lt` (ver: https://api.dentalink.healthatom.com/docs/#filtros-y-cursores)
+
+        Retorna:
+            Este mismo objeto, pero con el filtro `lt` agregado
+
+        Errores:
+            `dentalink.exceptions.DentalinkClientQueryError`: Arrojado en caso de que se intente agregar un filtro cuando no se ha configurado un campo.
+        """
         return self._add_filter("lt", value, dt_format)
 
     def lte(
@@ -95,14 +207,40 @@ class DentalinkQuery:
         value: Union[int, float, datetime, None] = None,
         dt_format: str = "%Y-%m-%d",
     ) -> "DentalinkQuery":
+        """Implementación del operador `lte` (ver: https://api.dentalink.healthatom.com/docs/#filtros-y-cursores)
+
+        Retorna:
+            Este mismo objeto, pero con el filtro `lte` agregado
+
+        Errores:
+            `dentalink.exceptions.DentalinkClientQueryError`: Arrojado en caso de que se intente agregar un filtro cuando no se ha configurado un campo.
+        """
         return self._add_filter("lte", value, dt_format)
 
     def lk(
         self, value: Union[datetime, None] = None, dt_format: str = "%Y-%m-%d"
     ) -> "DentalinkQuery":
+        """Implementación del operador `lk` (ver: https://api.dentalink.healthatom.com/docs/#filtros-y-cursores)
+
+        Retorna:
+            Este mismo objeto, pero con el filtro `lk` agregado
+
+        Errores:
+            `dentalink.exceptions.DentalinkClientQueryError`: Arrojado en caso de que se intente agregar un filtro cuando no se ha configurado un campo.
+        """
         return self._add_filter("lk", value, dt_format)
 
     def parse(self) -> dict[str, Union[dict[str, str], List[dict[str, str]]]]:
+        """Retorna los filtros en el formato que la API espera (ver: https://api.dentalink.healthatom.com/docs/#filtros-y-cursores)
+
+        Omite los campos sin filtros
+
+        Retorna:
+            Diccionario con los filtros en el formato de la API
+
+        Errores:
+            `dentalink.exceptions.DentalinkClientQueryError`: Arrojado en caso de que se intente llamar a este método sin haber configurado algún campo
+        """
         if len(self._query) == 0:
             raise DentalinkClientQueryError(
                 "No se ha proporcionado ningún campo para crear la consulta"
